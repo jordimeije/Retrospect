0344626bbeed622dbafb539fa6b14c7fcb6730f2
jordimeije <jordimeije@hotmail.com>
2019-03-19 23:36:08 +0100

    Version 0.9 update

---
+++ Retrospect/Assets/Scripts/Explosion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public GameObject Particle;
+    public int TotalHits;
+    public GameObject Portal;
+    public GameObject Sounds;
+
+    // Use this for initialization
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Floater" && col.transform.GetComponent<Valve.VR.InteractionSystem.PowerShot>().Touchable)
+        {
+            Particle.GetComponent<ParticleSystem>().Play();
+            Destroy(col.gameObject);
+            GetComponent<AudioSource>().Play();
+            TotalHits++;
+            if (TotalHits >= 3)
+            {
+                Portal.SetActive(true);
+                Sounds.GetComponent<AudioPlayer>().GateOpened(8);
+            }
+        }
+    }
+
+    /*private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Particle.GetComponent<ParticleSystem>().Play();
+            GetComponent<AudioSource>().Play();
+        }
+    }*/
+}
---
+++ Retrospect/Assets/TextDissapear.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDissapear : MonoBehaviour {
+    public float DissapearanceTime;
+    public float Speed;
+    public GameObject Player;
+    public float MaxDistance;
+    bool once = false;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(Player.transform.position, transform.position) < MaxDistance && once == false)
+        {
+            StartCoroutine(Dissapear());
+            once = true;
+        }
+    }
+
+    public IEnumerator Dissapear() {
+        print("Working");
+        yield return new WaitForSecondsRealtime(DissapearanceTime);
+        float Progress = 0;
+        while (GetComponent<Renderer>().material.GetFloat("_Fragmentation") < 1)
+        {
+            Progress += Time.deltaTime * Speed;
+            GetComponent<Renderer>().material.SetFloat("_Fragmentation", Mathf.Lerp(0, 1, Progress));
+            yield return new WaitForSecondsRealtime(Time.deltaTime);
+        }
+        Destroy(this.gameObject);
+    }
+}
