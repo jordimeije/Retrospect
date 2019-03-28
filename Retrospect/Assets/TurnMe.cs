ee7b4fa9f7fc27965386fb69547c49fc1d10bdbd
jordimeije <jordimeije@hotmail.com>
2019-03-28 16:56:31 +0100

    Added turnaround for infinite walk

---
+++ Retrospect/Assets/TurnMe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnMe : MonoBehaviour {
+    bool Test;
+    Vector3 _Pos;
+    Vector3 Pos;
+    public Transform Eye;
+    public Transform Parent;
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+            Parent.RotateAround(Eye.position, transform.up, -45); 
+
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            Parent.RotateAround(Eye.position, transform.up, 45);
+
+        }
+    }
+}
